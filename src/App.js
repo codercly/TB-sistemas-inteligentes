74278669541029b9b5d401120ccd4afecdad282a
codercly <rrclementirafael@gmail.com>
2023-10-21 19:26:48 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import logo from './logo.svg';
+import lirio from './images/lirio-da-paz.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={lirio} className="App-logo" alt="logo" />
+
+        
+        
+          <title>Cultivo do Lírio da Paz</title>
+         
+          <body>
+            <h1>Como Cultivar o Lírio da Paz</h1>
+
+            <p>Cultivar a planta Lírio da Paz (Spathiphyllum) é relativamente fácil, e esta planta é conhecida por ser uma das melhores opções para quem está começando a cuidar de plantas de interior, devido à sua resistência e capacidade de purificar o ar.</p>
+
+            <h2>Iluminação</h2>
+            <p>O Lírio da Paz gosta de luz indireta brilhante a moderada. Evite a luz solar direta, pois isso pode queimar suas folhas. Coloque-o perto de uma janela com cortina ou em uma área de luminosidade difusa.</p>
+
+            <h2>Rega</h2>
+            <p>Mantenha o solo úmido, mas não encharcado. Regue quando o topo do solo começar a secar. A frequência de rega pode variar de acordo com a temperatura, umidade e iluminação do ambiente. No geral, regue cerca de uma vez por semana, mas verifique o solo antes de regar.</p>
+
+            <h2>Umidade</h2>
+            <p>O Lírio da Paz gosta de um ambiente um pouco mais úmido. Pulverize as folhas regularmente ou coloque um prato com água perto da planta para aumentar a umidade ao redor.</p>
+
+            <h2>Fertilização</h2>
+            <p>Alimente a planta a cada dois meses durante a primavera e o verão com um fertilizante equilibrado. Não fertilize no inverno.</p>
+
+            <h2>Replantio</h2>
+            <p>O Lírio da Paz não precisa ser replantado com muita frequência. Faça isso quando as raízes começarem a ficar compactadas e saírem do vaso. Escolha um vaso ligeiramente maior do que o atual e use um solo para plantas de interior de boa qualidade.</p>
+
+            <h2>Poda</h2>
+            <p>Remova as folhas e flores mortas para manter a planta saudável e atraente.</p>
+
+            <h2>Temperatura</h2>
+            <p>O Lírio da Paz prefere temperaturas entre 18-24°C. Evite exposição a correntes de ar frio ou temperaturas extremas.</p>
+
+            <h2>Pragas e doenças</h2>
+            <p>Embora o Lírio da Paz seja resistente a pragas, ele pode ser suscetível a cochonilhas, ácaros ou pulgões. Verifique as folhas regularmente e trate qualquer infestação assim que a detectar.</p>
+
+            <h2>Divisão</h2>
+            <p>O Lírio da Paz pode ser dividido a cada dois a três anos para rejuvenescer a planta. Isso envolve retirar a planta do vaso, separar as partes saudáveis e replantá-las em vasos individuais.</p>
+
+
+          </body>
+      
+
+
       </header>
     </div>
   );
